b09001a6de02382d0751bcafead2f83bb3969d38
luson <ljhxai@163.com>
2015-10-30 13:02:45 +0800

    add check package

--- package.js
+++ package.js
@@ -1,68 +1,70 @@
 Package.describe({
-  name: "luson:admin",
-  summary: "A complete admin dashboard solution",
-  version: "0.1.0",
-  git: "https://github.com/ljhsai/meteor-admin"
+    name: "luson:admin",
+    summary: "A complete admin dashboard solution",
+    version: "0.1.0",
+    git: "https://github.com/ljhsai/meteor-admin"
 });
 
-Package.onUse(function(api){
+Package.onUse(function (api) {
 
-  both = ['client','server'];
+    both = ['client', 'server'];
 
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
+    api.versionsFrom('1.2.1');
+    api.use('ecmascript');
 
-  api.use(
-    ['iron:router',
-    'underscore',
-    'reactive-var',
-    'aldeed:collection2@2.5.0',
-    'aldeed:autoform@5.5.1',
-    'aldeed:template-extension@3.4.3',
-    'alanning:roles@1.2.13',
-    'raix:handlebar-helpers@0.2.5',
-    'reywood:publish-composite@1.4.2',
-    'momentjs:moment@2.10.6',
-    'aldeed:tabular@1.4.0',
-    'meteorhacks:unblock@1.1.0',
-    'zimme:active-route@2.3.2',
-    'mfactory:admin-lte'
-    ],
-    both);
+    api.use(
+        [
+            'iron:router',
+            'check',
+            'underscore',
+            'reactive-var',
+            'aldeed:collection2@2.5.0',
+            'aldeed:autoform@5.5.1',
+            'aldeed:template-extension@3.4.3',
+            'alanning:roles@1.2.13',
+            'raix:handlebar-helpers@0.2.5',
+            'reywood:publish-composite@1.4.2',
+            'momentjs:moment@2.10.6',
+            'aldeed:tabular@1.4.0',
+            'meteorhacks:unblock@1.1.0',
+            'zimme:active-route@2.3.2',
+            'mfactory:admin-lte'
+        ],
+        both);
 
-  api.use(['less@1.0.0 || 2.5.0','session','jquery','templating'],'client');
+    api.use(['less@1.0.0 || 2.5.0', 'session', 'jquery', 'templating'], 'client');
 
-  api.use(['email'],'server');
+    api.use(['email'], 'server');
 
-  api.add_files([
-    'lib/both/AdminDashboard.js',
-    'lib/both/router.js',
-    'lib/both/utils.js',
-    'lib/both/startup.js',
-    'lib/both/collections.js'
+    api.add_files([
+        'lib/both/AdminDashboard.js',
+        'lib/both/router.js',
+        'lib/both/utils.js',
+        'lib/both/startup.js',
+        'lib/both/collections.js'
     ], both);
 
-  api.add_files([
-    'lib/client/html/admin_templates.html',
-    'lib/client/html/admin_widgets.html',
-    'lib/client/html/admin_layouts.html',
-    'lib/client/html/admin_sidebar.html',
-    'lib/client/html/admin_header.html',
-    'lib/client/css/admin-custom.less',
-    'lib/client/js/admin_layout.js',
-    'lib/client/js/helpers.js',
-    'lib/client/js/templates.js',
-    'lib/client/js/events.js',
-    'lib/client/js/slim_scroll.js',
-    'lib/client/js/autoForm.js'
+    api.add_files([
+        'lib/client/html/admin_templates.html',
+        'lib/client/html/admin_widgets.html',
+        'lib/client/html/admin_layouts.html',
+        'lib/client/html/admin_sidebar.html',
+        'lib/client/html/admin_header.html',
+        'lib/client/css/admin-custom.less',
+        'lib/client/js/admin_layout.js',
+        'lib/client/js/helpers.js',
+        'lib/client/js/templates.js',
+        'lib/client/js/events.js',
+        'lib/client/js/slim_scroll.js',
+        'lib/client/js/autoForm.js'
     ], 'client');
 
-  api.add_files([
-    'lib/server/publish.js',
-    'lib/server/methods.js'
+    api.add_files([
+        'lib/server/publish.js',
+        'lib/server/methods.js'
     ], 'server');
 
-  api.export('AdminDashboard',both)
+    api.export('AdminDashboard', both)
 });
 
 Package.onTest(function (api) {
